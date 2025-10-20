bbd85789b5b053d0870d5af8d305a4a6f498fd97
StellarWasp-ian <avilaian2officialac@gmail.com>
2025-10-20 07:40:44 -0300

    punto 5

---
+++ punto5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int id;
+    char mensaje[30];
+    int tipo;
+	int idregis[10];
+    char mensajeregis[10];
+    int tiporegis[10];
+
+    printf("Ingrese el Mensaje: ");
+    scanf("%d");
+
+	return 0;
+}
