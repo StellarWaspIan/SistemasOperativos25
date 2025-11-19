972ec74c5a892c9fca6ae148aa9b86b1003b7d6d
StellarWasp-ian <avilaian2officialac@gmail.com>
2025-11-19 09:19:02 -0300

    movimientos

--- punto2.c
+++ punto2.c
@@ -1,31 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#define N 10 
 
-int main(){
+int main(void) {
+    int X[N]; 
+    int Y[N]; 
 
-	//funcion de comparacion para el ordenamiento
-	int compararChar(const void *a, const void *b) {
-    return (*(char *)a - *(char *)b);
-	}
-	char cadena1[30];
-    char cadena2[30];
-	printf("Ingrese la palabra 1: ");
-	scanf("%s", cadena1);
-	printf("Ingrese el palabra 2: ");
-	scanf("%s", cadena2);
-	
-	qsort(cadena1, strlen(cadena1), sizeof(char), compararChar);
-	qsort(cadena2, strlen(cadena2), sizeof(char), compararChar);
-
-	printf("Palabras Ordenadas: ");
-    printf("\n");
-	printf("Palabra 1: %s",cadena1);
-    printf("\n");
-	printf("Palabra 2: %s",cadena2);
-    printf("\n");
-	
-	return 0;
+    int i;
+    int pag, desp, dato;
+    int opcion;
+    for (i = 0; i < N; i++) {
+        X[i] = -1;
+        Y[i] = -1;
+    }
+    while (1) {
+        printf("\n=== Sistema de paginacion basico ===\n");
+        printf("1) Ingresar dato <pagina, desplazamiento, dato>\n");
+        printf("2) Visualizar paginas\n");
+        printf("0) Salir\n");
+        printf("Opcion: ");
+        if (scanf("%d", &opcion) != 1) {
+            printf("Entrada invalida.\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
+        if (opcion == 0) {
+            printf("Fin.\n");
+            break;
+        }
+        switch (opcion) {
+            case 1:
+                printf("Ingrese <pagina desplazamiento dato>: ");
+                if (scanf("%d %d %d", &pag, &desp, &dato) != 3) {
+                    printf("Entrada invalida.\n");
+                    int c;
+                    while ((c = getchar()) != '\n' && c != EOF);
+                    continue;
+                }
+                if (pag != 0 && pag != 1) {
+                    printf("Error: numero de pagina invalido (%d). Solo 0 o 1.\n", pag);
+                    break;
+                }
+                if (desp < 0 || desp >= N) {
+                    printf("Error: desplazamiento fuera de rango (0..%d).\n", N - 1);
+                    break;
+                }
+                if (pag == 0) {
+                    X[desp] = dato;
+                    printf("Se guardo %d en pagina 0 (X[%d]).\n", dato, desp);
+                } else {
+                    Y[desp] = dato;
+                    printf("Se guardo %d en pagina 1 (Y[%d]).\n", dato, desp);
+                }
+                break;
+            case 2:
+                printf("\nPagina 0 (vector X):\n");
+                for (i = 0; i < N; i++) {
+                    printf("X[%d] = %d\n", i, X[i]);
+                }
+                printf("\nPagina 1 (vector Y):\n");
+                for (i = 0; i < N; i++) {
+                    printf("Y[%d] = %d\n", i, Y[i]);
+                }
+                break;
+            default:
+                printf("Opcion invalida.\n");
+                break;
+        }
+    }
+    return 0;
 }
-
-
